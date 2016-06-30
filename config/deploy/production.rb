43ae72ba948084c9d399a4c5dce7b6629af733a1
luan293 <luan29394@gmail.com>
2016-06-30 12:54:17 +0700

    30/6

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,17 +1,17 @@
-user = 'vnits'
-ip_address = '13.76.88.104'
+#user = 'vnits'
+#ip_address = '13.76.88.104'
+ #roles: %w{web app}
+#role :app, ["#{user}@#{ip_address}"]
+#role :web, ["#{user}@#{ip_address}"]
+#role :db,  ["#{user}@#{ip_address}"]
  
-role :app, ["#{user}@#{ip_address}"]
-role :web, ["#{user}@#{ip_address}"]
-role :db,  ["#{user}@#{ip_address}"]
+#server ip_address,
+#       user: user,
+#       roles: %w{web app}
  
-server ip_address,
-       user: user,
-       roles: %w{web app}
+#set :rails_env, 'production'
  
-set :rails_env, 'production'
- 
-set :bundle_flags, '--no-deployment'
+#set :bundle_flags, '--no-deployment'
  
 #set :ssh_options, {
 #  keys: %w(~/.ssh/id_rsa_cqbinh.pub),
@@ -19,4 +19,4 @@ set :bundle_flags, '--no-deployment'
 #  port: 22
 #}
  
- set :nginx_server_name, 'demo_deploy.appconus.com'
+# set :nginx_server_name, 'demo_deploy.appconus.com'
